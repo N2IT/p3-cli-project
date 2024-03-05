86d327105edba240db065c859f442e2880beec63
Tony Eder <tonyeder11@gmail.com>
2024-03-05 01:25:20 -0800

    initial-commit

--- lib/cli.py
+++ lib/cli.py
@@ -5,8 +5,29 @@ from helpers import (
     helper_1
 )
 
+def login():
+    print("**************************")
+    username = input("Please enter your username: ")
+    print("**************************")
+    if username != "":
+            password(username)
+    else:
+        print(f'{username} is not recognized. Please try again.')
+        login()
 
-def main():
+def password(username):
+    print("**************************")
+    password = input("Please enter your password: ")
+    print("**************************")
+    if password != "":
+        main(username)
+    else:
+        (f'{password} is not recognized.')
+        password(username)
+
+
+def main(username):
+    print(f"Welcome {username}!")
     while True:
         menu()
         choice = input("> ")
@@ -17,12 +38,15 @@ def main():
         else:
             print("Invalid choice")
 
-
 def menu():
+    print("**************************")
     print("Please select an option:")
     print("0. Exit the program")
     print("1. Some useful function")
+    print("**************************")
+        
+
 
 
 if __name__ == "__main__":
-    main()
+    login()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,12 @@
 # lib/helpers.py
 
 def helper_1():
-    print("Performing useful function#1.")
-
+    print("performaing a function now")
 
 def exit_program():
     print("Goodbye!")
     exit()
+
+
+# def menu():
+#     choice = input("> ")
