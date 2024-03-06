6b12eb030e54913c0c69ed72de090dd196981ff8
Tony Eder <tonyeder11@gmail.com>
2024-03-05 16:50:20 -0800

    workout routine instances created, property decorators added

--- lib/models/workout_routine.py
+++ lib/models/workout_routine.py
@@ -1,12 +1,40 @@
-class WorkoutRoutine:
-    def __init__(self, name):
-        self.name = name
-        self.exercises = []
+from models.__init__ import CURSOR, CONN
 
-    def add_exercise(self, workout):
-        self.exercises.append(workout)
+class WorkoutRoutine:
+    
+    def __init__(self, title, equipment, id = None):
+        self.id = id
+        self.title = title
+        self.equipment = equipment
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter(self, title):
+        if len(title) > 20:
+            raise TypeError("Title must not exceed 20 characters.")
+        else:
+            self._title = title
+    
+    @property
+    def equipment(self):
+        return self._equipment
+
+    @equipment.setter
+    def equipment(self, equipment):
+        if len(equipment) > 25:
+            raise TypeError("Equipment must not exceed 25 characters ")
+        else:
+            self._equipment = equipment
+
+    def exercises(self):
+        # update here to pull from exercises table
+
+    def add_exercise(self, exercise):
+        if not isinstance(exercise, Exercise):
+            raise TypeError("Exercise must be of Exercise class.")
+        exercise.workout_routine = self
 
     def __repr__(self):
-        return f'Workout Routine: {self.name}\nExercises: {self.exercises}'
-
-    
+        return f'<Workout Routine {self.id}: {self.title}, {self.equipment}'
