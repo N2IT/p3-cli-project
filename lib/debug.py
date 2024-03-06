99cdb86bcd0100bdc74ba7fcf671e0cf126dcdfa
Tony Eder <tonyeder11@gmail.com>
2024-03-05 16:22:32 -0800

    follwing chat with instructor, making adjustments to simplify this project to complete.

--- lib/debug.py
+++ lib/debug.py
@@ -11,10 +11,10 @@ Exercise.drop_table()
 Exercise.create_table()
 
 
-# e1 = Exercise('Pushups', 'lie facing the floor and, keep back straight, raise body by pressing down on hands.')
-# e2 = Exercise('Planks', 'n isometric core strength exercise that involves maintaining a position similar to a push-up for the maximum possible time.')
-# e3 = Exercise('Pull Ups', 'Use your body weight to bulk up your back and biceps')
-# e4 = Exercise('Dips', 'Shred your chest and triceps with your body weight')
+e1 = Exercise.create('Pushups', 'lie facing the floor and, keep back straight, raise body by pressing down on hands.')
+e2 = Exercise.create('Planks', 'n isometric core strength exercise that involves maintaining a position similar to a push-up for the maximum possible time.')
+e3 = Exercise.create('Pull Ups', 'Use your body weight to bulk up your back and biceps')
+e4 = Exercise.create('Dips', 'Shred your chest and triceps with your body weight')
 
 
 ipdb.set_trace()
